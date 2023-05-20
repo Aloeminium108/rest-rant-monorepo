b196b8d4afadc26e961a0956c4924cff5db7d632
Alice Parrish <alice@pop-os.localdomain>
2023-05-20 12:50:53 -0400

    Added login functionality on backend

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -5,7 +5,19 @@ const bcrypt = require('bcrypt')
 const { User } = db
 
 router.post('/', async (req, res) => {
-    console.log('IN HERE')
+
+  const user = await User.findOne({
+    where: { email: req.body.email }
+  })
+
+  if (!user || !await bcrypt.compare(req.body.password, user.password_digest)) {
+    res.status(401).json({ 
+      message: "Incorrect email or password"
+    }) 
+  } else {
+    res.status(200).json({ user })
+  }
+
 })
 
 module.exports = router
